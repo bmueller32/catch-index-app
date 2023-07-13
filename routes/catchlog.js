1662b268011b1bf1c3c73e9a65a792df991ee4e6
Brent <brentmueller32@gmail.com>
2023-07-13 15:39:17 -0700

    import auth

--- routes/catchlog.js
+++ routes/catchlog.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
+const isLoggedIn = require('../config/auth')
 const catchlogCtrl = require('../controllers/catchlog');
 
 //GET /catchlog //define route
 router.get('/', catchlogCtrl.index),
 //GET /catchlog/new
-router.get('/new',catchlogCtrl.new);
+router.get('/new', isLoggedIn,catchlogCtrl.new);
 //GET /catchlog/:id (show) must be below new
 router.get('/:id', catchlogCtrl.show);
 // POST / catchlog
-router.post('/', catchlogCtrl.create);
+router.post('/', isLoggedIn, catchlogCtrl.create);
 //Get /catchlog/:id/edit
-router.get('/:id/edit', catchlogCtrl.edit)
+router.get('/:id/edit', isLoggedIn, catchlogCtrl.edit)
 //PUT /catchlog/:id
-router.put('/:id', catchlogCtrl.update);
+router.put('/:id', isLoggedIn, catchlogCtrl.update);
 
 
 
--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const isLoggedIn = require('../config/auth')
 const commentsCtrl = require('../controllers/comments');
 
 
-router.post('/catchlog/:id/comments', commentsCtrl.create)
-router.delete('/comments/:id', commentsCtrl.delete)
+router.post('/catchlog/:id/comments', isLoggedIn, commentsCtrl.create)
+router.delete('/comments/:id', isLoggedIn, commentsCtrl.delete)
 
 module.exports = router;
